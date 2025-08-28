7da260affb1f7d611f888f738ac70048f8ad0dd2
zc8131868 <zc8131868@gmail.com>
2025-08-28 16:51:56 +0800

    update the html files

--- auto_tickets/views/test.py
+++ auto_tickets/views/test.py
@@ -1,5 +1,18 @@
-import os
-print(os.getenv("TESTPASS"))
+import openpyxl
 
 
-"this is a test at 9:56"
+path = '/it_network/network_tickets/auto_tickets/ITSR_sample.xlsx'
+
+workbook = openpyxl.load_workbook(path)
+sheet = workbook.active
+
+for row in sheet.iter_rows(min_row=4, max_col=9):
+    source_ip_list = row[2].value
+    destination_ip = row[4].value
+    print(source_ip_list, destination_ip)
+
+
+
+
+
+
