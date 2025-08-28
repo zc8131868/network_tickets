7dac505cce2e0203fa30a893676ec1bf80c1f51e
zc8131868 <zc8131868@gmail.com>
2025-08-28 17:13:57 +0800

    where is my branch

--- network_tickets/test.py
+++ network_tickets/test.py
@@ -10,9 +10,32 @@
 # 提供完整的代码（Windows或者Linux）粘贴后注意行间距-参考作业标准
 # 对打印结果进行截图
 
+
+
+
+# import os
+# import re
+
+# route_n_result = os.popen("route -n").read()
+
+# gateway = re.search(r'0\.0\.0\.0\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+0\.0\.0\.0\s+UG', route_n_result)
+
+# print(f'网关为：{gateway.group(1)}')
+
+
+
+
 # 列表作业：L2是L1的排序（由小到大）
 
 # 作业标准:
 
 # 提供完整的代码（Windows或者Linux）粘贴后注意行间距-参考作业标准
 # 对打印结果进行截图
+
+
+l1 = [4,5,7,1,3,9,0]
+
+l2 = sorted(l1)
+
+for i in range(len(l1)):
+    print(l1[i], l2[i])
